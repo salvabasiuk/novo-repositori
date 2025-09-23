3e975f1f50034ffa3fa9798c5c6175f5f9055a5b
salvabasiuk <salvador.costa@escola.pr.gov.br>
2025-09-23 08:05:07 -0300

    perguntas certas

---
+++ perguntas.js
@@ -0,0 +1,71 @@
+criarCartao(
+    'Mcqeueen',
+    'Quem é Mcqueen?',
+    'É o carro protagonista do filme carros'
+
+)
+
+criarCartao(
+    'Qual é o maior sonho do Mcqueen?',
+    'Vencer a copa pistão'
+
+)
+
+criarCartao(
+    'quem se torna o grande amigo de Mcqueen no filme?',
+    'O guincho'
+
+)
+
+criarCartao(
+    'qual a principal lição que Mcqueen aprende no primeiro filme ?',
+    'Ele aprende que a amizade e a humildade são mais importantes do que Vencer'
+
+)
+
+criarCartao(
+    'quem e a treinadora de Mcqueen em carros 3',
+    'Cruz ramirez'
+
+)
+
+criarCartao(
+    'qual rivalidade marcou o inicio da carreira de Mcqueen?',
+    'a disputa contra o rei strip Weathers chick hicks'
+
+)
+
+criarCartao(
+    ' como Mcqueen foi para em radiator springs',
+    'ele se perdeu no caminho para a corrida final e acabou danificando a estrada'
+)
+
+criarCartao(
+    'qual o numero do Mcqueen nas corridas',
+    'ele usa o numero 95'
+
+)
+
+criarCartao(
+    'Quem se torna o grande amor de Mcqueen',
+    'sally carreira'
+
+)
+
+criarCartao(
+    'qual a maior dificuldade enfrentada por Mcqueen?',
+    'enfrentar a nova geração de carros de corrida'
+)
+
+criarCartao(
+    'Mcqueen sempre vence suas corridas?',
+    'Não. ele aprende que perde tambem faz parte'
+
+)
+
+criarCartao(
+    'O que simboliza a evolução de Mcqueen nos filmes?',
+    'a passagem de um joven arrogante para um carro sabio e generos'
+)
+
+
